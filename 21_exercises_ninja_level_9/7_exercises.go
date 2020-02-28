5e3bb8d80af5ae4e32ff2824fc0680a5e93431e5
Richard Chernanko <richard@applike.info>
2020-02-28 07:53:05 +0100

    add random exercise

--- 21_exercises_ninja_level_9/7_exercises.go
+++ 21_exercises_ninja_level_9/7_exercises.go
@@ -1,1 +1,37 @@
 package main
+
+/*
+
+Won't do this at the moment. Maybe come back to it one day...
+
+Download OBS. Create a screen recording teaching some aspect of the Go programming language. Some topics you might teach:
+Why Go
+installing go
+GOROOT & GOPATH
+environment variables
+hello world
+go commands
+help
+variables
+Short declaration operator
+constants
+loops
+init, cond, post
+break
+continue
+functions
+func (receiver) identifier(params) (returns) { code }
+methods
+interfaces
+method sets
+type
+conversion
+concurrency vs parallelism
+goroutines
+waitgroups
+mutex
+atomic
+
+After creating your recording, upload your video to youtube. Then tweet a link to your video and tag me in the tweet so that I can see it.
+
+*/
