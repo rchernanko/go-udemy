00d109292f7cfcdf6f68c7ee86e9b2bbb822cd67
Richard Chernanko <richard@applike.info>
2020-03-25 20:52:23 +0100

    go modules

---
+++ 3_development_environment/1.go
@@ -0,0 +1,14 @@
+package __development_environment
+
+/**
+
+Went through a lot of this - just watched the videos
+
+Go modules:
+
+- Dependency management
+- Create a go.mod file with the `go mod init` command
+- I've created a new repo for this - just to have it a little cleaner. More notes in there
+- https://github.com/rchernanko/go-modules-messing-around
+
+*/
--- 3_development_environment/come_back_to_this.go
+++
@@ -1,7 +0,0 @@
-package __development_environment
-
-/**
-
-Went through a lot of this - just watched the videos
-
-*/
